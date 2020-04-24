412fa168e59de0912dbb61ed1d85d3e768b86683
tinnerrss <tinnerrss@hotmail.com>
2020-04-24 15:37:51 -0700

    initial commit

---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+    return (
+        <div className="Landing">
+
+        </div>
+    )
+}
+
+export default Landing;
---
+++ src/components/LetsConnect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LetsConnect() {
+    return (
+        <div className="letsconnect">
+
+        </div>
+    )
+}
+
+export default LetsConnect;
